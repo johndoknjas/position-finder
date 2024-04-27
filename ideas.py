johndoknjas/position-finder

commit 53644b0ca3d7cb5d17361c01e237361cf553ca7c

Fix some mypy errors, and update the gitignore with standard python stuff.

main.py
@@ -164,7 +164,7 @@ def satisfies_bound(move_dict: dict, bound: Optional[float], is_lower_bound: boo
         else:
             return move_dict["Centipawn"] <= bound
 
-def does_position_satisfy_bounds(stockfish: Stockfish, fen: str, bounds: List[float]) -> bool:
+def does_position_satisfy_bounds(stockfish: Stockfish, fen: str, bounds: List[Optional[float]]) -> bool:
     """bounds is a list, where the 0th element is the lower bound for the first move,
        the 1st element is the upper bound for the first move, etc (for however many
        top moves). It may just be the one top move, or it could be 1 more, 2 more, etc.
@@ -178,7 +178,7 @@ def does_position_satisfy_bounds(stockfish: Stockfish, fen: str, bounds: List[fl
     # rather than positive being white and negative being black.
     for depth in depth_increments:
         stockfish.set_depth(depth)
-        top_moves: List[dict] = stockfish.get_top_moves(len(bounds) / 2)
+        top_moves: List[dict] = stockfish.get_top_moves(int(len(bounds) / 2))
         if len(top_moves) != len(bounds) / 2:
             return False
         for i in range(len(top_moves)):
@@ -293,7 +293,7 @@ def print_output_data(type_of_position: str, output_string: str, secondary_outpu
                 "\nHit counter: " + str(hit_counter) + "\n\n")
         f.close()
 
-def main():
+def main() -> None:
     output_filename = str(int(time.time()))
     
     type_of_position = input("""Enter 'endgame', 'top moves', 'skip move', or 'underpromotion' for the type of position to find: """)
@@ -307,6 +307,7 @@ the last name of White, then a space, then the last name of Black, then a space,
 To not do this, just press enter: """)
     
     if game_to_start_search_after != "":
+        assert type(game_to_start_search_after) is str
         name_of_player_as_white_in_first_game = game_to_start_search_after.split()[0]
         name_of_player_as_black_in_first_game = game_to_start_search_after.split()[1]
         date_of_first_game = game_to_start_search_after.split()[2]
@@ -363,16 +364,19 @@ enter it here. Otherwise, just press enter: """) or "0")
     while True:
         if not reached_first_game_for_search_in_DB:
             headers = chess.pgn.read_headers(pgn)
+            assert headers is not None
+            game_date = headers.get("Date")
+            assert type(game_date) is str
             reached_first_game_for_search_in_DB = (
                 (   
                     name_of_player_as_white_in_first_game in headers.get("White", "?") and 
                     name_of_player_as_black_in_first_game in headers.get("Black", "?") and
-                    date_of_first_game in headers.get("Date")
+                    date_of_first_game in game_date
                 ) 
                 if type(game_to_start_search_after) is str
                 else 
                 (
-                    num_games_parsed >= game_to_start_search_after
+                    num_games_parsed >= game_to_start_search_after # type: ignore
                 )
             )
             if reached_first_game_for_search_in_DB:

.gitignore
@@ -3,3 +3,164 @@
 stockfish*
 *.ini
 *.txt
+
+# Byte-compiled / optimized / DLL files
+__pycache__/
+*.py[cod]
+*$py.class
+
+# C extensions
+*.so
+
+# Distribution / packaging
+.Python
+build/
+develop-eggs/
+dist/
+downloads/
+eggs/
+.eggs/
+lib/
+lib64/
+parts/
+sdist/
+var/
+wheels/
+share/python-wheels/
+*.egg-info/
+.installed.cfg
+*.egg
+MANIFEST
+
+# PyInstaller
+#  Usually these files are written by a python script from a template
+#  before PyInstaller builds the exe, so as to inject date/other infos into it.
+*.manifest
+*.spec
+
+# Installer logs
+pip-log.txt
+pip-delete-this-directory.txt
+
+# Unit test / coverage reports
+htmlcov/
+.tox/
+.nox/
+.coverage
+.coverage.*
+.cache
+nosetests.xml
+coverage.xml
+*.cover
+*.py,cover
+.hypothesis/
+.pytest_cache/
+cover/
+
+# Translations
+*.mo
+*.pot
+
+# Django stuff:
+*.log
+local_settings.py
+db.sqlite3
+db.sqlite3-journal
+
+# Flask stuff:
+instance/
+.webassets-cache
+
+# Scrapy stuff:
+.scrapy
+
+# Sphinx documentation
+docs/_build/
+
+# PyBuilder
+.pybuilder/
+target/
+
+# Jupyter Notebook
+.ipynb_checkpoints
+
+# IPython
+profile_default/
+ipython_config.py
+
+# pyenv
+#   For a library or package, you might want to ignore these files since the code is
+#   intended to run in multiple environments; otherwise, check them in:
+# .python-version
+
+# pipenv
+#   According to pypa/pipenv#598, it is recommended to include Pipfile.lock in version control.
+#   However, in case of collaboration, if having platform-specific dependencies or dependencies
+#   having no cross-platform support, pipenv may install dependencies that don't work, or not
+#   install all needed dependencies.
+#Pipfile.lock
+
+# poetry
+#   Similar to Pipfile.lock, it is generally recommended to include poetry.lock in version control.
+#   This is especially recommended for binary packages to ensure reproducibility, and is more
+#   commonly ignored for libraries.
+#   https://python-poetry.org/docs/basic-usage/#commit-your-poetrylock-file-to-version-control
+#poetry.lock
+
+# pdm
+#   Similar to Pipfile.lock, it is generally recommended to include pdm.lock in version control.
+#pdm.lock
+#   pdm stores project-wide configurations in .pdm.toml, but it is recommended to not include it
+#   in version control.
+#   https://pdm.fming.dev/#use-with-ide
+.pdm.toml
+
+# PEP 582; used by e.g. github.com/David-OConnor/pyflow and github.com/pdm-project/pdm
+__pypackages__/
+
+# Celery stuff
+celerybeat-schedule
+celerybeat.pid
+
+# SageMath parsed files
+*.sage.py
+
+# Environments
+.env
+.venv
+env/
+venv/
+ENV/
+env.bak/
+venv.bak/
+
+# Spyder project settings
+.spyderproject
+.spyproject
+
+# Rope project settings
+.ropeproject
+
+# mkdocs documentation
+/site
+
+# mypy
+.mypy_cache/
+.dmypy.json
+dmypy.json
+
+# Pyre type checker
+.pyre/
+
+# pytype static type analyzer
+.pytype/
+
+# Cython debug symbols
+cython_debug/
+
+# PyCharm
+#  JetBrains specific template is maintained in a separate JetBrains.gitignore that can
+#  be found at https://github.com/github/gitignore/blob/main/Global/JetBrains.gitignore
+#  and can be added to the global gitignore or merged into this file.  For a more nuclear
+#  option (not recommended) you can uncomment the following to ignore the entire idea folder.
+#.idea/

commit 3c5379736d97d72745de2a7d500effaf95b7228e

For the `name` feature, just iterate through the game headers (very fast). Also fix a bug for the skipping games feature, which was introduced in 53644b0c.

ideas.py
@@ -3,6 +3,9 @@
   number.
     - Maybe ask the user for a min and max.
 - Clean up the code a bit, wherever seems necessary.
+    - E.g., put the for loop that iterates over moves into its own function.
+- Make a pytest file in order to do regression tests. Been multiple times now where a change has broken something,
+  so good to have tests.
 - When opening the pgn file, consider using 'with open'.
 - When the Stockfish fork is eventually on PyPI, and updated so that mypy can analyze it, import it again for
   this project instead of using models.py directly.

main.py
@@ -312,7 +312,7 @@ enter it here. Otherwise, just press enter: """) or "0")
         or "0"
     )
 
-    DEFAULT_OUTPUT_INTERVALS = 40 if type_of_position != "endgame" else 200
+    DEFAULT_OUTPUT_INTERVALS = {'endgame': 200, 'name': 40000}.get(type_of_position, 40)
 
     if type_of_position == "endgame":
         num_pieces_desired_endgame = int(user_input) if (user_input :=
@@ -358,14 +358,12 @@ enter it here. Otherwise, just press enter: """) or "0")
         num_games_parsed += 1
         if not reached_first_game_for_search_in_DB:
             headers = chess.pgn.read_headers(pgn)
-            assert headers is not None
-            game_date = headers.get("Date")
-            assert type(game_date) is str
             reached_first_game_for_search_in_DB = (
                 (
+                    headers is not None and
                     name_of_player_as_white_in_first_game in headers.get("White", "?") and
                     name_of_player_as_black_in_first_game in headers.get("Black", "?") and
-                    date_of_first_game in game_date
+                    date_of_first_game in headers.get("Date", "?")
                 )
                 if isinstance(game_to_start_search_after, str)
                 else
@@ -378,83 +376,82 @@ enter it here. Otherwise, just press enter: """) or "0")
             if num_games_parsed % 20000 == 0:
                 print("Skipped " + str(num_games_parsed))
             continue
-        current_game = chess.pgn.read_game(pgn)
-        if current_game is None:
-            break
-        current_game_as_str = str(current_game)
-        board = current_game.board()
-        move_counter = 0
-        prev_move = None
+
         newest_hit = ""
+        if type_of_position == 'name':
+            if (headers := chess.pgn.read_headers(pgn)) is None:
+                break
+            white, black = (headers.get(x, '?') for x in ("White", "Black"))
+            if any(x.lower() in y.lower() for x, y in itertools.product(name_contains, (white, black))):
+                output_string += (newest_hit := f"{white}-{black}\n\n----------\n\n")
+                hit_counter += 1
+        else:
+            current_game = chess.pgn.read_game(pgn)
+            if current_game is None:
+                break
+            current_game_as_str = str(current_game)
+            board = current_game.board()
+            move_counter = 0
+            prev_move = None
+            for move in current_game.mainline_moves():
+                if newest_hit:
+                    print_output_data(type_of_position, output_string, secondary_output_string,
+                                    hit_counter, secondary_hit_counter, num_games_parsed, output_filename,
+                                    newest_hit)
+                newest_hit = ""
 
-        for move in current_game.mainline_moves():
-            if newest_hit:
-                print_output_data(type_of_position, output_string, secondary_output_string,
-                                  hit_counter, secondary_hit_counter, num_games_parsed, output_filename,
-                                  newest_hit)
-            newest_hit = ""
+                if type_of_position == "underpromotion":
+                    if prev_move is not None:
+                        board.push(prev_move)
+                    prev_move = move # Note - prev_move is a misnomer for the rest of this loop iteration now.
+                else:
+                    board.push(move)
+                move_counter += 1
+                if move_counter < move_to_start_in_each_game * 2:
+                    continue
+                board_str_rep = board.fen() + "\n" + str(board) + "\nfrom:\n" + current_game_as_str
 
-            if type_of_position == "underpromotion":
-                if prev_move is not None:
-                    board.push(prev_move)
-                prev_move = move # Note - prev_move is a misnomer for the rest of this loop iteration now.
-            else:
-                board.push(move)
-            move_counter += 1
-            if move_counter < move_to_start_in_each_game * 2:
-                continue
-            board_str_rep = board.fen() + "\n" + str(board) + "\nfrom:\n" + current_game_as_str
+                if type_of_position == "endgame":
+                    num_pieces_in_current_fen = num_pieces_in_fen(board.fen())
+                    if (num_pieces_desired_endgame is not None and
+                        num_pieces_in_current_fen < num_pieces_desired_endgame):
+                        break  # Too few pieces to ever reach the desired endgame now.
+                    if (    (num_pieces_desired_endgame is None or
+                            num_pieces_in_current_fen == num_pieces_desired_endgame)
+                        and does_position_satisfy_specs(stockfish, board.fen(), endgame_specs)):
+                        newest_hit = board_str_rep + "\n\n----------\n\n"
+                        output_string += newest_hit
+                        hit_counter += 1
+                        break  # On to the next game
 
-            if type_of_position == "endgame":
-                num_pieces_in_current_fen = num_pieces_in_fen(board.fen())
-                if (num_pieces_desired_endgame is not None and
-                    num_pieces_in_current_fen < num_pieces_desired_endgame):
-                    break  # Too few pieces to ever reach the desired endgame now.
-                if (    (num_pieces_desired_endgame is None or
-                         num_pieces_in_current_fen == num_pieces_desired_endgame)
-                    and does_position_satisfy_specs(stockfish, board.fen(), endgame_specs)):
-                    newest_hit = board_str_rep + "\n\n----------\n\n"
-                    output_string += newest_hit
-                    hit_counter += 1
-                    break  # On to the next game
-
-            elif type_of_position == "top moves":
-                if does_position_satisfy_bounds(stockfish, board.fen(), bounds):
-                    newest_hit = (board_str_rep + "\nTop moves:\n" +
-                                  ', '.join(str(d) for d in stockfish.get_top_moves(2)) +
-                                  "\n\n----------\n\n")
-                    output_string += newest_hit
-                    hit_counter += 1
-
-            elif type_of_position == "skip move":
-                if (does_position_satisfy_bounds(stockfish, board.fen(), bounds[0:2]) and
-                    stockfish.is_fen_valid(switch_whose_turn(board.fen())) and
-                    does_position_satisfy_bounds(stockfish, switch_whose_turn(board.fen()),
-                                                 bounds[2:4])):
-                    newest_hit = board_str_rep + "\n\n----------\n\n"
-                    output_string += newest_hit
-                    hit_counter += 1
-
-            elif type_of_position == "underpromotion":
-                underpromotion_move = is_underpromotion_best(stockfish, board.fen()) # returns False or str
-                if underpromotion_move: # must be a move string
-                    newest_hit = board_str_rep + "\n\n----------\n\n"
-                    secondary_output_string += newest_hit
-                    secondary_hit_counter += 1
-                    if underpromotion_move != move.uci():
+                elif type_of_position == "top moves":
+                    if does_position_satisfy_bounds(stockfish, board.fen(), bounds):
+                        newest_hit = (board_str_rep + "\nTop moves:\n" +
+                                    ', '.join(str(d) for d in stockfish.get_top_moves(2)) +
+                                    "\n\n----------\n\n")
                         output_string += newest_hit
                         hit_counter += 1
 
-            elif type_of_position == 'name':
-                white, black = (current_game.headers[x] for x in ("White", "Black"))
-                if any(x.lower() in y.lower() for x, y in itertools.product(name_contains, (white, black))):
-                    newest_hit = f"{current_game.headers['White']}-{current_game.headers['Black']}"
-                    newest_hit += "\n\n----------\n\n"
-                    output_string += newest_hit
-                    hit_counter += 1
-                    break
+                elif type_of_position == "skip move":
+                    if (does_position_satisfy_bounds(stockfish, board.fen(), bounds[0:2]) and
+                        stockfish.is_fen_valid(switch_whose_turn(board.fen())) and
+                        does_position_satisfy_bounds(stockfish, switch_whose_turn(board.fen()),
+                                                    bounds[2:4])):
+                        newest_hit = board_str_rep + "\n\n----------\n\n"
+                        output_string += newest_hit
+                        hit_counter += 1
 
-        # End of for loop for iterating over the moves of the current game
+                elif type_of_position == "underpromotion":
+                    underpromotion_move = is_underpromotion_best(stockfish, board.fen()) # returns False or str
+                    if underpromotion_move: # must be a move string
+                        newest_hit = board_str_rep + "\n\n----------\n\n"
+                        secondary_output_string += newest_hit
+                        secondary_hit_counter += 1
+                        if underpromotion_move != move.uci():
+                            output_string += newest_hit
+                            hit_counter += 1
+
+                # End of for loop for iterating over the moves of the current game
 
         if newest_hit or num_games_parsed % DEFAULT_OUTPUT_INTERVALS == 0:
             print_output_data(type_of_position, output_string, secondary_output_string,
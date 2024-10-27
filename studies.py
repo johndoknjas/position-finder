fbb2aee162e39ba96eda0479071bb6788b5545f1
johndoknjas <jdoknjas@sfu.ca>
2024-10-27 02:36:36 -0700

    Raise a runtime error if the status code from the lichess api response is not 200.

--- studies.py
+++ studies.py
@@ -6,7 +6,10 @@ def get_api_key() -> str:
         return f.readline().strip('\n')
 
 def get_study_pgn(study_id) -> str:
-    return requests.get(
+    response = requests.get(
         f'https://lichess.org/api/study/{study_id}.pgn?source=true',
         headers={"Authorization": f"Bearer {get_api_key()}"}
-    ).text
+    )
+    if response.status_code != 200:
+        raise RuntimeError(f"lichess api response status code is {response.status_code}")
+    return response.text
